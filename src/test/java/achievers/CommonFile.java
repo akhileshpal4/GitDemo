85c7f268952d127c2ad0fe89b6d1e4aeb387632f
unknown <akhileshpal.pal4@gmail.com>
2024-03-23 16:47:11 +0530

    added commonFile.java

---
+++ src/test/java/achievers/CommonFile.java
@@ -0,0 +1,11 @@
+package achievers;
+
+import org.testng.annotations.Test;
+
+public class CommonFile {
+    @Test
+    public void method(){
+        System.out.println("Akhilesh");
+        System.out.println("Kumar");
+    }
+}
