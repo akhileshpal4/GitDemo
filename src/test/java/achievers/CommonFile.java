37d086687862ddd42d23ef15e049d53d4f9ed41f
unknown <akhileshpal.pal4@gmail.com>
2024-03-23 17:44:59 +0530

    j to 15

--- src/test/java/achievers/CommonFile.java
+++ src/test/java/achievers/CommonFile.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class CommonFile {
     @Test
     public void method(){
-        int j=12;
+        int j=15;
         int i=10;
         System.out.println("Rupali");
         System.out.println("Kumar");
